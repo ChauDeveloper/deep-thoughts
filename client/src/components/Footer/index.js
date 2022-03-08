775a009b54ff5dcf98270a5d62f7da099ef29202
chaudeveloper <ms_michanguyen@yahoo.com>
2022-03-08 15:48:21 -0500

    added client part while creating an app to run both server and client site simutaneously instead of running them seperately

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2022 by ChauDeveloper</div>
+    </footer>
+  );
+};
+
+export default Footer;
